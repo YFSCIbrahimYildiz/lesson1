822f075dbb6c9cd9543db0fac202d84e6b8ac4b5
İbrahim <yldzibrhim@gmail.com>
2024-08-04 23:55:30 +0300

    Lesson1

--- app/src/main/java/com/yfscribrahimapp/javalesson1/lesson.java
+++ app/src/main/java/com/yfscribrahimapp/javalesson1/lesson.java
@@ -5,5 +5,31 @@ public class lesson {
     public static void main(String[] args){
         int age=20;
         System.out.println(15*age);
+
+        double a=5.0;
+        double b=11.0;
+        System.out.println(a/b);
+        float fakefloat=10.0f;
+        double pi=3.14;
+        int r=5;
+
+        System.out.println(2*pi*r);
+        //String
+        String text="Hello Word";
+        String text1="Android için Java Öğrenme :)";
+
+        System.out.println(text+" "+text1);
+        //Boolean
+        boolean isAlive=true;
+        isAlive=false;
+        System.out.println(isAlive);
+
+        //Final
+        int myInteger =5;
+        System.out.println(myInteger);
+        myInteger=4;
+        System.out.println(myInteger);
+
+
     }
 }
